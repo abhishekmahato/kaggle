0a1f421d2b9e79432aa0bcb16dc0bea25033d07e
Abhishek <abhishek.loyolean@gmail.com>
2020-04-27 14:05:00 +0530

    edX course codes

--- Titanic/code_practice.R
+++ Titanic/code_practice.R
@@ -163,3 +163,124 @@ gapminder %>%
 #5.2.1 Show data
 heights %>% ggplot(aes(sex, height)) +
   geom_jitter(width = 0.1, alpha = 0.2)
+
+#5.3.3 Show 3 variables
+library(RColorBrewer)
+data(us_contagious_diseases)
+the_disease <- "Measles"
+dat <- us_contagious_diseases %>%
+  filter(!state %in% c("Hawaii", "Alaska") & disease == the_disease) %>%
+  mutate(rate = count / population * 10000 * 52/weeks_reporting) %>%
+  mutate(state = reorder(state, rate))
+
+dat %>% filter (state == 'California' & !is.na(rate)) %>%
+  ggplot(aes(year, rate)) +
+  geom_line() + 
+  geom_vline(xintercept = 1963, colour = 'green')+
+  ylab("cases per 10,000")
+
+dat %>% filter(!is.na(rate)) %>% ggplot(aes(year, state, fill = rate)) +
+  geom_tile(color = 'grey50')+
+  scale_x_continuous(expand = c(0,0)) +
+  scale_fill_gradientn(colors = RColorBrewer::brewer.pal(9, 'Reds'), trans = 'sqrt') +
+  theme(panel.grid = element_blank())+
+  geom_vline(xintercept = 1963)
+
+#titanic Assignment
+options(digits = 3)    # report 3 significant digits
+library(tidyverse)
+library(titanic)
+#install.packages('titanic')
+#Question 2
+train = titanic_train
+str(train)
+for(i in 1:length(train)){
+  if ( train[i] %>% is.na() %>% which() %>% length() != 0 )
+    print(names(train[i]))
+}
+#train[i] %>% is.na() %>% which() %>% length() == 0
+#length(which(is.na(train[, i]))) !=0
+summary(train)
+head(train)
+
+train %>% ggplot(aes(x = Age, fill = Sex)) +
+  geom_density(alpha = 0.2)+
+  facet_grid(Sex ~ .)
+
+train %>% ggplot(aes(x = Age, y = after_stat(count))) +
+  geom_density(alpha = 0.2) +
+  scale_x_continuous(trans = 'log2') +
+  facet_grid(Sex ~ .)
+  geom_vline(xintercept = 17) +
+  geom_vline(xintercept = 35)
+
+train %>% ggplot(aes(x = Age, y = after_stat(count), fill = Sex)) +
+  geom_density(alpha = 0.2, bw = 0.75) +
+  scale_x_continuous(trans = 'log2')+
+  scale_y_continuous(trans = 'log2')
+
+train %>% group_by(Sex) %>% filter(Age == 40) %>%
+summarize(max = max(Age, na.rm = TRUE), n=n()) %>% 
+  knitr::kable()
+
+age_group = train %>% filter (Age <= 17) %>% group_by(Sex) %>%
+  summarize(n = n())
+total = train %>% group_by(Sex) %>% 
+  summarize(n=n())
+age_group$n/total$n
+
+#Question 3
+params <- train %>%
+  filter(!is.na(Age)) %>%
+  summarize(mean = mean(Age), sd = sd(Age))
+
+p = seq(0,0.99,0.01)
+observed = quantile(train$Age, p, na.rm = TRUE)
+ggplot() +
+  geom_qq(aes(sample = observed), dparams = params, na.rm = TRUE)+
+  geom_abline()
+  
+#Question 4
+train %>% ggplot(aes(Survived, fill = Sex)) +
+  geom_bar(position = position_dodge())
+
+train %>% ggplot(aes(Sex, fill = factor(Survived))) +
+  geom_bar()
+
+train %>% group_by(Survived) %>%
+  summarize(prop = n()/(train %>% summarize(n =n()))$n) %>%
+  knitr::kable()
+
+#Question 5
+train %>%
+  ggplot(aes(Age, y = after_stat(count), fill = factor(Survived))) +
+  geom_density(alpha = 0.2) +
+  scale_x_continuous(breaks = c(0, 8, 18, 30, 50, 70, 80)) +
+  geom_vline(xintercept = c(0, 8, 18, 30, 50, 70, 80))
+
+#Question 6 
+train %>% filter(Fare != 0) %>%
+  ggplot(aes(x = Survived, y = Fare)) +
+  geom_boxplot(aes(group = Survived)) +
+  geom_jitter(alpha = 0.2, width = 0.2) + 
+  scale_y_continuous(trans = 'log2')
+
+#Question 7
+train %>% ggplot(aes(Pclass, fill = factor(Survived))) +
+  geom_bar()
+
+train %>% ggplot(aes(Survived, fill = factor(Pclass))) +
+  geom_bar(position = position_fill())
+
+train %>% group_by(Pclass, Survived) %>% 
+  summarize(prop = n()) %>%
+  knitr::kable()
+
+#Question 8
+train %>% filter(!is.na(Age)) %>%
+  ggplot(aes(Age, fill = factor(Survived))) +
+  geom_density(aes(y = after_stat(count), alpha = 0.2)) +
+  facet_grid(Sex ~ Pclass)
+
+
+
